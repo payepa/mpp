56f56bd765c1fc009ac34ab190dd3edaa3d5e393
Your Name <payepa@proton.me>
2025-07-29 20:32:15 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request, url_for
+import requests
+
+app = Flask(__name__)
+
+BASE_URL = "https://mppapi.vercel.app/"
+# BASE_URL = "http://localhost:8000/" # Local testing
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+@app.route("/search")
+def search():
+    query = request.args.get('q')
+    if query:
+        results = requests.get(f"{BASE_URL}search", params={ 'q': query }).json()['results']
+
+    return render_template('search.html', mangas=results)
+
+@app.route("/info")
+def info():
+    manga_id = request.args.get('id')
+    info = requests.get(f"{BASE_URL}info?id={manga_id}").json()
+    
+    return render_template('info.html', manga=info)
+
+@app.route("/read")
+def read():
+    manga_id = request.args.get('id')
+    chapter = request.args.get('chapter') or 1
+
+    manga = requests.get(f"{BASE_URL}read?id={manga_id}&chapter={chapter}").json()
+
+    return render_template('read.html', manga=manga)
+
+app.run(debug=True)
+
